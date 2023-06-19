8f9353acce7ac4f69486cfe05471acace0fba494
VictorEmanuel08 <victorem3181@gmail.com>
2023-06-18 22:45:34 -0300

    Att

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import "../styles/style.scss";
 import Logo from "../assets/Tattoo CollectioN-1.png";
+import logoPng from "../assets/Tattoo Collection.png";
 
-import { AiFillFacebook, AiOutlineInstagram } from "react-icons/ai";
+import { AiOutlineInstagram, AiFillPlusCircle } from "react-icons/ai";
 import { Link } from "react-router-dom";
 
 export function Home() {
@@ -17,41 +18,158 @@ export function Home() {
   const scrollToDiv = (index) => {
     divsRefs[index].current.scrollIntoView({ behavior: "smooth" });
   };
+
+  const [imageNamesTatto, setImageNamesTatto] = useState([]);
+  const [imagesUserTatto, setImagesUserTatto] = useState([]);
+  const [imageNamesCorpo, setImageNamesCorpo] = useState([]);
+  const [imagesUserCorpo, setImagesUserCorpo] = useState([]);
+
+  const handleFileChange = (event) => {
+    const files = event.target.files;
+    // Salvando o nome das imagens para printar depois
+    const newNames = Array.from(files).map((file) => file.name);
+    setImageNamesTatto((prevNames) => [...prevNames, ...newNames]);
+
+    // Salvando as imagens no vetor para enviar
+    const readerArray = [];
+
+    for (let i = 0; i < files.length; i++) {
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        readerArray[i] = reader.result;
+        if (readerArray.length === files.length) {
+          setImagesUserTatto([...imagesUserTatto, ...readerArray]);
+        }
+      };
+
+      reader.readAsDataURL(files[i]);
+    }
+  };
+
   return (
-    <div
-      className="page"
-      // className="w-full bg-black flex flex-col"
-    >
-      <section className="section1" ref={divsRefs[0]}>
-        <div class="left">
-          <div class="image-container">
-            <img src={Logo} alt="Imagem" />
+    <div className="page">
+      <section className="section0" ref={divsRefs[0]}>
+        <div className="left">
+          <div className="image-container">
+            <img className="logo-section0" src={Logo} alt="Imagem" />
           </div>
         </div>
-        <div class="right">
-          <div class="links-container">
-            <div onClick={() => scrollToDiv(1)} class="link">
-              <div class="link-content">Agende sua tatto</div>
+        <div className="right">
+          <div className="links-container">
+            <div onClick={() => scrollToDiv(1)} className="link">
+              <div className="link-content">
+                <span>Nosso portfolio</span>
+              </div>
             </div>
-            <div onClick={() => scrollToDiv(2)} class="link">
-              <div class="link-content">Localização</div>
+            <div onClick={() => scrollToDiv(2)} className="link">
+              <div className="link-content">
+                <span>Agende sua tatto</span>
+              </div>
             </div>
-            <div class="link">
-              <div class="link-content">Nosso portfolio</div>
+            <div onClick={() => scrollToDiv(3)} className="link">
+              <div className="link-content">
+                <span>Localização</span>
+              </div>
             </div>
-            <div class="link">
-              <div class="link-content">Sobre nós</div>
+            <div className="link">
+              <div className="link-content">
+                <span>Sobre nós</span>
+              </div>
             </div>
-            <div class="link link-with-icons">
-              <AiFillFacebook class="icon" size="2rem" />
-              <AiOutlineInstagram class="icon" size="2rem" />
+            <div className="link-with-icons">
+              <Link
+                to="https://instagram.com"
+                target="_blank"
+                style={{ color: "inherit" }}
+              >
+                <AiOutlineInstagram className="icon" />
+              </Link>
             </div>
           </div>
         </div>
       </section>
-      <section className="section2" ref={divsRefs[1]}></section>
-      <section className="section3" ref={divsRefs[2]}></section>
-      <section className="section4" ref={divsRefs[3]}></section>
+      <section className="section1" ref={divsRefs[1]}>
+        PORTIFOLIO
+      </section>
+      <section className="section2" ref={divsRefs[2]}>
+        <div className="image-logo">
+          <img className="logo-sections" src={logoPng} alt="Logo" />
+        </div>
+        <form className="form-container">
+          <div className="form-items">
+            <label htmlFor="nome">Nome Completo</label>
+            <input className="form-input" type="text" placeholder="Nome" />
+          </div>
+          <div className="form-group">
+            <div className="form-items" style={{ marginRight: "0.5rem" }}>
+              <label htmlFor="email">E-mail</label>
+              <input className="form-input" type="email" placeholder="E-mail" />
+            </div>
+            <div className="form-items" style={{ marginLeft: "0.5rem" }}>
+              <label htmlFor="whatsApp">WhatsApp</label>
+              <input
+                className="form-input"
+                type="text"
+                placeholder="WhatsApp"
+              />
+            </div>
+          </div>
+          <span className="form.text">
+            Abaixo você deixara a inspiração de sua tatuagem e posição do corpo
+            no qual será feito.
+          </span>
+          <div className="form-group">
+            <div className="form-items" style={{ marginRight: "0.5rem" }}>
+              <label htmlFor="tattoo" className="file-label">
+                Referencia da tatto
+                <AiFillPlusCircle className="file-icon" />
+                <input
+                  id="tattoo"
+                  type="file"
+                  className="form-input"
+                  accept="image/*"
+                  multiple
+                  onChange={handleFileChange}
+                />
+              </label>
+              {/* VERIFICAR MULTIPLE
+              VERIRICAR O MARGIN DOS NAMES */}
+              <div className="file-names">
+                {imageNamesTatto.map((file, index) => {
+                  return (
+                    <span key={index} className="file-name">
+                      {file}
+                    </span>
+                  );
+                })}
+              </div>
+            </div>
+            <div className="form-items" style={{ marginLeft: "0.5rem" }}>
+              <label htmlFor="corpo" className="file-label">
+                Parte do corpo
+                <AiFillPlusCircle className="file-icon" />
+              </label>
+              <input
+                id="corpo"
+                type="file"
+                className="form-input"
+                accept="image/*"
+              />
+            </div>
+          </div>
+          <input type="date" placeholder="Data" />
+          <button type="submit" className="submit-button">
+            Enviar
+          </button>
+        </form>
+      </section>
+      <section className="section3" ref={divsRefs[3]}>
+        LOCALIZAÇÃO
+      </section>
+      <section className="section4" ref={divsRefs[4]}>
+        SOBRE NÓS
+      </section>
     </div>
   );
 }
